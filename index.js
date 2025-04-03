0ec47ebe8c822d61d6a811b3284e24ac45b0b07a
1alusha1 <alusha9636@gmail.com>
2025-04-03 11:21:28 +0300

    added opportunity check several domains

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
-const sendTelegramNotification = async (status) => {
+const sendTelegramNotification = async (status, domain) => {
   const token = process.env.BOT_TOKEN; // Твой токен Telegram бота
   const chatId = "-4740863276"; // Чат, куда отправлять сообщения
 
   await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
     chat_id: chatId,
-    text: `Domain status: ${status} at ${new Date().toLocaleString()}`,
+    text: `Domain ${domain} status: ${status} at ${new Date().toLocaleString()}`,
   });
 };
 
+function extractDomain(url) {
+  const regex = /^(?:https?:\/\/)?(?:www\.)?([^\/]+)/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
 exports.checkDomain = async (req, res) => {
   try {
-    await axios.get("domain");
-    await sendTelegramNotification("up");
+    const domains = [];
+
+    domains.forEach(async (domain) => {
+      await axios.get(domain);
+      await sendTelegramNotification("up", extractDomain(domain));
+    });
   } catch (error) {
     await sendTelegramNotification("down");
   }
